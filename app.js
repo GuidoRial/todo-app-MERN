d4e0080051e7081cd8454e1ef9d97d0b514f1e8c
GuidoRial <rialguido@gmail.com>
2022-05-09 14:27:46 -0300

    deploy

--- app.js
+++ app.js
@@ -33,10 +33,10 @@ app.use(notFoundMiddleware);
 app.use(notFoundMiddleware);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.resolve(__dirname, "./client/build")));
+    app.use(express.static(path.join(__dirname, "./client/build")));
     app.get("*", function (request, response) {
         response.sendFile(
-            path.resolve(__dirname, "./client/build", "index.html")
+            path.join(__dirname, "./client/build", "index.html")
         );
     });
 }
