7292d9caa499efde94f7b57e29ef30c3454da82f
GuidoRial <rialguido@gmail.com>
2022-05-16 07:37:49 -0300

    deleted dist file

--- dist/middleware/not-found.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var notFound = function (req, res) {
-    return res.status(404).send("Route does not exist");
-};
-exports.default = notFound;
